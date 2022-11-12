8d4aebbd74f119609926e1323d775a440e543492
Slijeff <jeff83636tx@gmail.com>
2022-11-12 14:10:12 -0600

    feat: update navbar

--- src/components/navbarButton.tsx
+++ src/components/navbarButton.tsx
@@ -1,14 +1,26 @@
-import { ActionIcon, Button, Flex, Text } from '@mantine/core';
+import { ActionIcon, Button, Card, Flex, Text } from '@mantine/core';
 import { FunctionComponent } from 'react';
 import { IconPaw, TablerIcon } from '@tabler/icons';
+import { useNavigate } from 'react-router-dom';
 interface NavbarButtonProps {
   text: string;
   icon: React.ReactNode;
+  routeTo: string;
 }
 
-const NavbarButton: FunctionComponent<NavbarButtonProps> = ({ text, icon }) => {
+const NavbarButton: FunctionComponent<NavbarButtonProps> = ({
+  text,
+  icon,
+  routeTo,
+}) => {
+  const navigate = useNavigate();
   return (
-    <Flex direction={'column'} justify={'center'} align={'center'}>
+    <Flex
+      direction={'column'}
+      justify={'center'}
+      align={'center'}
+      onClick={() => navigate(routeTo)}
+    >
       <ActionIcon variant='transparent'>{icon}</ActionIcon>
       <Text color={'white'} size={'xs'}>
         {text}
