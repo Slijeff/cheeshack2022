62c66320cc9fb59f9f087c30826f525d9c7f1691
Slijeff <jeff83636tx@gmail.com>
2022-11-12 20:27:07 -0600

    feat: test deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cheeshack2022/',
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 6789,
   },
 });
